5c868374772035f3602d9e624241586d70684ce4
merkoland-dot <merkou@gmail.com>
2026-02-24 11:17:38 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,21 @@
-
-const CACHE_NAME = "snow-route-cache-v1";
-const urlsToCache = [
-  "index.html",
-  "manifest.json"
-];
+const CACHE_NAME = "snow-route-cache-v2";
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ]);
+    })
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
