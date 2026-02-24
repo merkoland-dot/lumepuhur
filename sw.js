75267bae3ee65e9ab99f636d81a181256c4b9079
merkoland-dot <merkou@gmail.com>
2026-02-24 14:23:27 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,48 @@
-const CACHE_NAME = "snow-route-cache-v3";
+const CACHE_NAME = "snow-route-cache-v4";
 
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./app.js"
+];
+
+/* INSTALL */
 self.addEventListener("install", event => {
+  self.skipWaiting(); // aktiveerib kohe uue versiooni
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
+      return cache.addAll(ASSETS);
     })
   );
 });
 
+/* ACTIVATE */
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+/* FETCH */
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, clone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
